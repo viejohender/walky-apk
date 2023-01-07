3feb41a224a43c14dca2422abc3695b8e4613cb5
viejohender <hender.javier@hotmail.com>
2023-01-07 17:29:59 -0500

    aap-walky

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
-
+import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
 import { HomePageRoutingModule } from './home-routing.module';
 
 
@@ -14,6 +14,8 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  providers: [InAppBrowser],
+  declarations: [HomePage],
+  
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
+import { App } from '@capacitor/app';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  constructor(private browser: InAppBrowser) {}
+
+  ngOnInit(): void {
+    const browserInstance = this.browser.create("https://app.walky.com.co/", "_self","location=no,hidenavigationbuttons=true,hideurlbar=true,zoom=no");
+    browserInstance.on("exit").subscribe(evt => {
+      App.exitApp();
+    } );
+  }
 
 }
